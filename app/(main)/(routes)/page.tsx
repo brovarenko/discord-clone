89dd3ac4cc5ec33fa7cf4aa834a9d440ff238d8b
vladyslav-brovarenko <brovwg@gmail.com>
2023-12-10 14:06:49 +0200

    added auth

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className='flex flec-col'>
       <p className='text-3xl font=bold text-indigo-500'>Hello discord clone</p>
+      <UserButton afterSignOutUrl='/' />
     </div>
   );
 }
